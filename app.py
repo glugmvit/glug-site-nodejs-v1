c4f07fc8fbfc88a8616dff9d96d15d8f8b554482
abhijith0505 <abhijith4m0505@gmail.com>
2016-10-29 16:08:26 +0530

    500 error handler added

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def page_not_found(e):
     return render_template('error.html')
 
+#Internal server error, 500, can be tested only in non-debug mode
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('error.html'), 500
