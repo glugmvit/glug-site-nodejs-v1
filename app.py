c7c4fb394e37c6d72d8f803aac91754db734b6c4
abhijith0505 <abhijith4m0505@gmail.com>
2016-10-30 19:51:40 +0530

    Pep8 compliant

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ app = Flask(__name__)
 
 connection = MongoClient("< your mongodb url >")
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error.html')
 
-#Internal server error, 500, can be tested only in non-debug mode
+# Internal server error, 500, can be tested only in non-debug mode
+
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('error.html'), 500
+
 
 @app.route('/', methods=['GET'])
 def index():
